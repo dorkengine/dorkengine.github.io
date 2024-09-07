8ecceb0993b1ab9e8436c0111c9af7ad515c8061
Suraj S <110619000+voizok@users.noreply.github.com>
2024-09-07 22:31:58 +0530

    v1.0

--- src/script.js
+++ src/script.js
@@ -1,14 +1,12 @@
 function runDork(dork) {
     const domain = document.getElementById('domainInput').value;
     if (domain) {
-        // Replace 'example.com' with the actual domain
         const finalDork = dork.replace('example.com', domain);
         
-        // Check if the dork is a URL that should be opened directly
         if (finalDork.startsWith('http://') || finalDork.startsWith('https://')) {
             window.open(finalDork, '_blank');
         } else {
-            // Otherwise, treat it as a Google search query
+
             window.open(`https://www.google.com/search?q=${encodeURIComponent(finalDork)}`, '_blank');
         }
     } else {
